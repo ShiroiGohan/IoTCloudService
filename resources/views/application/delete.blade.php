e15500d80bd57d7feb60513c29fd852f0eb6f491
ShiroiGohan <i@hanayo.moe>
2017-12-16 19:34:41 +0900

    modified:   public/css/app.css 	modified:   public/js/app.js 	modified:   resources/views/application/delete.blade.php 	modified:   resources/views/application/edit.blade.php 	modified:   resources/views/application/index.blade.php 	modified:   resources/views/application/new.blade.php 	modified:   resources/views/application/view.blade.php 	modified:   resources/views/static/index.blade.php

--- resources/views/application/delete.blade.php
+++ resources/views/application/delete.blade.php
@@ -6,11 +6,15 @@
 
 @section('content')
   <div class="container">
-    <h1> Form goes here.</h1>
+    <h1>Confirmation</h1>
+    <h2>{{ $application -> name }} will be deleted. Sure?</h2>
+    </br>
     <form class="form" method="post">
       <input name="_method" type="hidden" value="delete">
       {{ csrf_field() }}
-      <input type="submit"></input>
+      <button type="submit" class="btn btn-danger">Delete</button>
+      &nbsp;
+      <a href="{{ route('application_view', ['id' => $application -> id ]) }}"><button type="button" class="btn btn-success">Cancel</button></a> 
     </form>
   </div>
 @endsection
